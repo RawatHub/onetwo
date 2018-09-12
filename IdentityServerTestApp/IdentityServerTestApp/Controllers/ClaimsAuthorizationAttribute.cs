16377fd21b5302e143aeb01a6c101d1f3476ee42
Umak <xinnovationlab@gmail.com>
2018-09-13 00:41:58 +0530

    Added claim based authorization

---
+++ IdentityServerTestApp/IdentityServerTestApp/Controllers/ClaimsAuthorizationAttribute.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Net.Http;
+
+namespace IdentityServerTestApp.Controllers
+{
+    public class ClaimsAuthorizationAttribute : AuthorizationFilterAttribute
+    {
+        public string ClaimType { get; set; }
+        public string ClaimValue { get; set; }
+
+        public override Task OnAuthorizationAsync(HttpActionContext actionContext, System.Threading.CancellationToken cancellationToken)
+        {
+
+            var principal = actionContext.RequestContext.Principal as ClaimsPrincipal;
+
+            if (!principal.Identity.IsAuthenticated)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                return Task.FromResult<object>(null);
+            }
+
+            if (!(principal.HasClaim(x => x.Type == ClaimType && x.Value == ClaimValue)))
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                return Task.FromResult<object>(null);
+            }
+
+            //User is Authorized, complete execution
+            return Task.FromResult<object>(null);
+
+        }
+    }
+
+    public class ClaimsAuthorization2Attribute : AuthorizationFilterAttribute
+    {
+        public override Task OnAuthorizationAsync(HttpActionContext actionContext, System.Threading.CancellationToken cancellationToken)
+        {
+            var principal = actionContext.RequestContext.Principal as ClaimsPrincipal;
+
+            if (!principal.Identity.IsAuthenticated)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                return Task.FromResult<object>(null);
+            }
+
+            string CustomerName = HttpContext.Current.Request.QueryString["CustomerName"];
+
+            //string CustomerName = HttpContext.Current.Request.QueryString["CustomerName"];
+
+            //string CustomerName = actionContext.Request.GetQueryNameValuePairs().FirstOrDefault(x => x.Key == "CustomerName").Value;
+
+            //string CustomerName = actionContext.ActionArguments["CustomerName"].ToString(); ;
+
+            if (!(principal.HasClaim(x => x.Type == "CustomerName" && x.Value == CustomerName)))
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                return Task.FromResult<object>(null);
+            }
+
+            //User is Authorized, complete execution
+            return Task.FromResult<object>(null);
+
+        }
+    }
+
+    public class ApiAuthorizationFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            var principal = actionContext.RequestContext.Principal as ClaimsPrincipal;
+
+            if (!principal.Identity.IsAuthenticated)
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                base.OnActionExecuting(actionContext);
+            }
+
+            string CustomerName = actionContext.ActionArguments["CustomerName"].ToString();
+
+            if (!(principal.HasClaim(x => x.Type == "CustomerName" && x.Value == CustomerName)))
+            {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                base.OnActionExecuting(actionContext);
+            }
+
+            //User is Authorized, complete execution
+            base.OnActionExecuting(actionContext);
+        }
+    }
+}
--- ProBI.Identity/ProBI.Identity/Controllers/AccountsController.cs
+++ ProBI.Identity/ProBI.Identity/Controllers/AccountsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNet.Identity;
+using ProBI.Identity.Managers.Providers;
 using ProBI.Identity.Models.ApplicationModels;
 using ProBI.Identity.Models.BindingModels;
 using System;
@@ -189,6 +190,65 @@ namespace ProBI.Identity.Controllers
             {
                 ModelState.AddModelError("", "Failed to add user roles");
                 return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("user/{id:guid}/assignclaims")]
+        [HttpPut]
+        public async Task<IHttpActionResult> AssignClaimsToUser([FromUri] string id, [FromBody] List<ClaimBindingModel> claimsToAssign)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var appUser = await this.AppUserManager.FindByIdAsync(id);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            foreach (ClaimBindingModel claimModel in claimsToAssign)
+            {
+                if (appUser.Claims.Any(c => c.ClaimType == claimModel.Type))
+                {
+
+                    await this.AppUserManager.RemoveClaimAsync(id, ExtendedClaimsProvider.CreateClaim(claimModel.Type, claimModel.Value));
+                }
+
+                await this.AppUserManager.AddClaimAsync(id, ExtendedClaimsProvider.CreateClaim(claimModel.Type, claimModel.Value));
+            }
+
+            return Ok();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [Route("user/{id:guid}/removeclaims")]
+        [HttpPut]
+        public async Task<IHttpActionResult> RemoveClaimsFromUser([FromUri] string id, [FromBody] List<ClaimBindingModel> claimsToRemove)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var appUser = await this.AppUserManager.FindByIdAsync(id);
+
+            if (appUser == null)
+            {
+                return NotFound();
+            }
+
+            foreach (ClaimBindingModel claimModel in claimsToRemove)
+            {
+                if (appUser.Claims.Any(c => c.ClaimType == claimModel.Type))
+                {
+                    await this.AppUserManager.RemoveClaimAsync(id, ExtendedClaimsProvider.CreateClaim(claimModel.Type, claimModel.Value));
+                }
             }
 
             return Ok();
